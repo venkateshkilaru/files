e7da5f4b9f7c2dfa98aec7a63b77810e2c34f050
venkateshkilaru <46456369+venkateshkilaru@users.noreply.github.com>
2019-01-08 00:10:12 +0530

    demo1
    demo1

---
+++ bank.py
@@ -0,0 +1,12 @@
+class bank:
+    def assign (self):
+        self.depositer_name="venkatesh"
+        self.depositer_acno=123456789
+        self.depositer_actype="savings"
+        self.depositer_blns=15000
+        self.deposit_amount=input("deposit amount:")
+        
+    
+    def diplay(self):
+       
+        print(self.
